caa81c4f7b1306b3d4191ceec67367089e87fb55
Sancho McCann <sanchom@gmail.com>
2023-09-01 19:06:39 -0700

    Added license metadata: MIT.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 
 (define collection "pollen")
+(define license '(MIT))
 (define test-omit-paths '("info.rkt" "scribblings/citations-mcgill.scrbl"))
 (define scribblings '(("scribblings/citations-mcgill.scrbl")))
 (define deps '("base"
